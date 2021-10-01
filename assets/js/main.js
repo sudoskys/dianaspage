80f2b4a3291ec05ca128ff3266b5732f0e7261a0
luoying2024 <75739606+luoying2024@users.noreply.github.com>
2021-10-01 15:38:24 +0800

    2.0版本
    APPLY to show random-passage

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   reset(1);
 });
 function reset(init) {
-  
   if (init === 1) {
     $("#app-result-card").hide();
   } else {
@@ -15,18 +14,34 @@ $("#reset").click(function () {
   console.log("reset");
   return false;
 });
+var lim = 0
+
 $("#generate-form").submit(function () {
+  lim +=1
   d = $(this).serializeArray();
   var data = new Array();
   $.each(d, function (i, field) {
     data[field.name] = field.value;
   });
-  var text =
-    '<p>不关注然然的人有难了[番茄炒蛋拳]</p>';
-  var result = juicer(text, data);
-  // console.log(result);
-  $("#app-result").html(result);
-  $("#app-result-card").fadeIn();
-
+  var page = Math.floor(Math.random() * 66)
+  fetch('https://asoulcnki.asia/v1/api/ranking/?pageSize=10&pageNum='+page+'&timeRangeMode=0&sortMode=0')
+  	.then(response => response.json())
+  	.then(data => { 
+  		// const contents = document.getElementById('data')
+  		// 不懂JS的人写JS有多难.PNG
+  		var n = Math.floor(Math.random() * 10)
+  	    var text = data["data"]["replies"][n]["content"]+"\n  By - "+data["data"]["replies"][n]["m_name"]
+		if(lim<20) {
+		var result = juicer(text, data);
+		//console.log(result);
+		$("#app-result").html(result);
+		$("#app-result-card").fadeIn();
+			}else{
+		window.open("https://space.bilibili.com/672328094")
+		}
+		
+		})
+  	.catch(console.error) 
+ 
   return false;
 });
